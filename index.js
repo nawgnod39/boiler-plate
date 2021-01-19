c943529d0908d2a3b938f9d142db475bdcff2614
takdongwan <dongwantak@gmail.com>
2021-01-19 23:53:20 +0900

    회원 가입 틀

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express =require ("express")
 const app = express()
 const port = 5000
 
-
+const {User} = require()
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://takdongwan:ekfrrkfql1@boiler-plate-shard-00-00.8rzdm.mongodb.net:27017,boiler-plate-shard-00-01.8rzdm.mongodb.net:27017,boiler-plate-shard-00-02.8rzdm.mongodb.net:27017/boiler-plate?ssl=true&replicaSet=atlas-zozl20-shard-0&authSource=admin&retryWrites=true&w=majority',{
@@ -12,4 +12,13 @@ mongoose.connect('mongodb://takdongwan:ekfrrkfql1@boiler-plate-shard-00-00.8rzdm
 
 
 app.get('/',(req,res)=>res.send('hello world'))
+
+
+app.post('/register',(req,res)=>{
+    //회원 가입 할 때 필요한정보 가져오면 그것을 데이터 베이스에 넣어준다.
+    
+}
+
+
+)
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
