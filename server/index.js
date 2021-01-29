f639efbe58d0b657f85dc27b49dd924f527b76cd
takdongwan <dongwantak@gmail.com>
2021-01-29 19:10:41 +0900

    proxy 설정

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 app.get('/', (req, res) => res.send('Hello World!~~ '))
+app.get('/api/hello', (req, res) => res.send('Hello World!~~ '))
 
 
 
